b17babe37c798d42031f385b4e88505a10f3a540
EzraDryerBU <edryer1@binghamton.edu>
2021-07-09 02:04:32 -0500

    more work on text boxes

--- Assets/Scripts/FloatingText.cs
+++ Assets/Scripts/FloatingText.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class FloatingText : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class FloatingText {
+
+    public bool active;
+    public GameObject go;
+    public Text txt;
+    public Vector3 motion;
+    public float duration;
+    public float lastShown;
+
+    public void Show() {
+        active = true;
+        lastShown = Time.time;
+        go.SetActive(active);
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Hide() {
+        active = false;
+        go.SetActive(active);
     }
+
+    public void UpdateFloatingText() {
+
+        if (!active) return;
+
+        if(Time.time - lastShown > duration) {
+            Hide();
+        }
+
+        go.transform.transform.position += motion * Time.deltaTime;
+
+    }
+
 }
